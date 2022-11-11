7db68540cf4f9e7c1b5f3da364cb4ac224602932
Santiago Diaz Monvale <santiago.diaz.monvale@gmail.com>
2022-11-11 16:49:24 -0300

    stylesheets hotfix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import './index.css';
-import "./Stylesheets.js"
+import "./stylesheets.js"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
