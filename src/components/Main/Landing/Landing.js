022f5cf53e7ae15dbad94776356cd4303d8b1c9b
Santiago Diaz Monvale <105252205+MonvaFNK@users.noreply.github.com>
2022-12-01 15:29:56 -0300

    Centering landing text + colors

--- src/components/Main/Landing/Landing.js
+++ src/components/Main/Landing/Landing.js
@@ -1,6 +1,3 @@
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Button from 'react-bootstrap/Button';
 import Marca from './Utilities/Marca.png';
 import Nightsky from './Nightsky/Nightsky.js';
 import Typewriter from 'typewriter-effect';
@@ -17,25 +14,33 @@ function Landing() {
                 <div className="maincontainer__texto--titular">
                     <h1 className='maincontainer__texto--titular1'>NAMI<img src={Marca} className='maincontainer__texto--marca' /><span className="maincontainer__texto--titular2">NIMA</span></h1>
                 </div>
-                <div className='maincontainer__texto2'>
-                    <h3 className="maincontainer__texto--parrafo">Potenciamos tu
-                        <Typewriter
-                            options={{
-                                strings: ['negocio', 'empresa', 'marca'],
-                                autoStart: true,
-                                skipAddStyles: false,
-                                loop: true,
-                                cursor: '',
-                            }}
-                        />   </h3>
-                    <h3 className='maincontainer__texto--parrafo'>en<Typewriter
-                        options={{
-                            strings: ['Facebook', 'Instagram', 'YouTube'],
-                            autoStart: true,
-                            loop: true,
-                            cursor: '',
-                        }}
-                    /></h3>
+                <div className='maincontainer__texto'>
+                    <h3 className="maincontainer__texto--parrafo">
+                        <span className="maincontainer__texto--parrafo_oraciones1">
+                            <span className='fix-right'> Potenciamos tu</span>
+                            <Typewriter
+                                options={{
+                                    strings: ['negocio', 'empresa', 'marca'],
+                                    autoStart: true,
+                                    skipAddStyles: false,
+                                    loop: true,
+                                    cursor: '',
+                                    color: '#00FFFF',
+                                }}
+                            />
+                        </span>
+                        <span className="maincontainer__texto--parrafo_oraciones2">
+                            <span className='fix-right'> en</span>
+                            <Typewriter
+                                className='nima'
+                                options={{
+                                    strings: ['Facebook', 'Instagram', 'YouTube'],
+                                    autoStart: true,
+                                    loop: true,
+                                    cursor: '',
+                                }}
+                            /></span>
+                    </h3>
                 </div>
             </div>
             <div className="maincontainer__imagenes--seccion">
